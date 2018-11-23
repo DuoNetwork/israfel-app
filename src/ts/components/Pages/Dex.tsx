b10f12887505dc3313bf969b3eb1ac47ed9a9cc2
yizhoucao <yizhou.cao@finbook.co>
2018-11-23 18:30:09 +0800

    try sign object

--- src/ts/components/Pages/Dex.tsx
+++ src/ts/components/Pages/Dex.tsx
@@ -2,6 +2,7 @@ import { Layout, Spin } from 'antd';
 import { Affix } from 'antd';
 import * as React from 'react';
 import { IEthBalance, IOrderBookSnapshot, ITokenBalance, IUserOrder } from 'ts/common/types';
+import web3Util from 'ts/common/web3Util';
 import Header from 'ts/containers/HeaderContainer';
 import { SDivFlexCenter } from '../_styled';
 import OrderBookCard from '../Cards/OrderBookCard';
@@ -90,7 +91,54 @@ export default class Dex extends React.Component<IProps> {
 							</div>,
 							<div key={4} style={{ color: 'white' }}>
 								<pre>{JSON.stringify(tokenBalance, null, 4)}</pre>
-							</div>
+							</div>,
+							<div key={5} style={{ color: 'white' }}>
+							<button
+								onClick={() =>
+									web3Util.web3Wrapper
+										.signTypedDataAsync(account, {
+											types: {
+												EIP712Domain: [
+													{ name: 'name', type: 'string' },
+													{ name: 'version', type: 'string' },
+													{ name: 'chainId', type: 'uint256' },
+												],
+												Person: [
+													{ name: 'name', type: 'string' },
+													{ name: 'wallet', type: 'address' }
+												],
+												Mail: [
+													{ name: 'from', type: 'Person' },
+													{ name: 'to', type: 'Person' },
+													{ name: 'contents', type: 'string' }
+												]
+											},
+											primaryType: 'Mail',
+											domain: {
+												name: 'Ether Mail',
+												version: '1',
+												chainId: 42,
+												verifyingContract:
+													'0xCcCCccccCCCCcCCCCCCcCcCccCcCCCcCcccccccC'
+											},
+											message: {
+												from: {
+													name: 'Cow',
+													wallet: '0xCD2a3d9F938E13CD947Ec05AbC7FE734Df8DD826'
+												},
+												to: {
+													name: 'Bob',
+													wallet: '0xbBbBBBBbbBBBbbbBbbBbbbbBBbBbbbbBbBbbBBbB'
+												},
+												contents: 'Hello, Bob!'
+											}
+										})
+										.then(result => console.log(result))
+								}
+							>
+								Sign
+							</button>
+						</div>
 						]
 					) : (
 						<Spin />
