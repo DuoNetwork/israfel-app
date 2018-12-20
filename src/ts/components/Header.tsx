cfb2b7aa18e9d40995a39a407ff2ee113c3f5f96
Wen Sihao <wensihao822@gmail.com>
2018-12-20 15:32:40 +0800

    Add exchange price tags

--- src/ts/components/Header.tsx
+++ src/ts/components/Header.tsx
@@ -2,13 +2,14 @@ import duoIcon from 'images/DUO_icon.png';
 import * as React from 'react';
 import { Link } from 'react-router-dom';
 import * as CST from 'ts/common/constants';
+import util from 'ts/common/util';
 import { SDivFlexCenter, SHeader } from './_styled';
 //import LocaleSelect from './Common/LocaleSelect';
 
 export interface IProps {
 	network: number;
 	locale: string;
-	exchangePrices: { [source: string]: number }
+	exchangePrices: { [source: string]: number };
 	updateLocale: (locale: string) => any;
 }
 
@@ -16,6 +17,9 @@ export default class Header extends React.Component<IProps> {
 	public render() {
 		const { network, exchangePrices } = this.props;
 		const locale = this.props.locale || CST.LOCALE_EN;
+		const exPx = [];
+		for (const key in exchangePrices) exPx.push({ exchange: key, price: exchangePrices[key] });
+		console.log(exPx);
 		return (
 			<SHeader>
 				<SDivFlexCenter horizontal width={'1200px'} height={'60px'}>
@@ -25,15 +29,34 @@ export default class Header extends React.Component<IProps> {
 								<img src={duoIcon} />
 							</div>
 						</Link>
+						<div className="nav-bal-wrapper">
+							{network ? (
+								(__KOVAN__ && network !== CST.NETWORK_ID_KOVAN) ||
+								(!__KOVAN__ && network !== CST.NETWORK_ID_MAIN) ? (
+									<span className="error-msg">
+										{CST.TT_NETWORK_CHECK[locale]}
+									</span>
+								) : (
+									exPx.map(ex => (
+										<div className="nav-bal-item">
+											<div key={ex.exchange}>{ex.exchange}</div>
+											<div key={ex.price} className="nav-bal-value">
+												{util.formatPriceShort(ex.price)}
+											</div>
+										</div>
+									))
+								)
+							) : null}
+						</div>
 					</div>
-					{network ? (
+					{/* {network ? (
 						(__KOVAN__ && network !== CST.NETWORK_ID_KOVAN) ||
 						(!__KOVAN__ && network !== CST.NETWORK_ID_MAIN) ? (
 							<span className="error-msg">{CST.TT_NETWORK_CHECK[locale]}</span>
 						) : (
 							JSON.stringify(exchangePrices)
 						)
-					) : null}
+					) : null} */}
 					<SDivFlexCenter horizontal>
 						<div className="nav-button-wrapper">{CST.DB_ORDERS.toUpperCase()}</div>
 						<div className="nav-button-wrapper">
