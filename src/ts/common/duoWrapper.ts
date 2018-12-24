e56ea0fbe6a85788a1545b6b47ba5c3c69dd5dfe
peiru.li@finbook.co <peiru.li@finbook.co>
2018-12-24 15:44:37 +0800

    update test

--- src/ts/common/duoWrapper.ts
+++ src/ts/common/duoWrapper.ts
@@ -2,9 +2,16 @@
 import '@babel/polyfill';
 import DualClassWrapper from '../../../../duo-contract-wrapper/src/DualClassWrapper';
 import Web3Wrapper from '../../../../duo-contract-wrapper/src/Web3Wrapper';
-import infura from '../../../../israfel-relayer/src/keys/infura.json';
 import * as CST from './constants';
 
+let infura = {
+	token: ''
+};
+try {
+	infura = require('../../../israfel-relayer/src/keys/infura.json');
+} catch (error) {
+	console.log(error);
+}
 const provider =
 	(__ENV__ === CST.DB_LIVE ? CST.PROVIDER_INFURA_MAIN : CST.PROVIDER_INFURA_KOVAN) + '/' + infura.token;
 export const duoWeb3Wrapper = new Web3Wrapper(window, '', provider, __ENV__ === CST.DB_LIVE);
