b4fd334142b9e52fa3daadff13fdfd3ae1f2f70e
peiru.li@finbook.co <peiru.li@finbook.co>
2019-01-10 12:18:55 +0800

    FIN-246 #comment test

--- src/ts/components/Cards/BalanceCard.test.tsx
+++ src/ts/components/Cards/BalanceCard.test.tsx
@@ -12,10 +12,51 @@ describe('BalanceCard Test', () => {
 	describe('Test Snapshot', () => {
 		const visible = true;
 		const ethPrice = 123;
-		const beethovenList = ['123', 'test'];
-		const mozartList = ['test', '123'];
+		const beethovenList = [
+			'0x00be45fe5903ab1b33a9d3969b05b29552a6d18b',
+			'0x00be45fe5903ab1b33a9d3969b05b29552a6d18c'
+		];
+		const mozartList = [
+			'0x00be45fe5903ab1b33a9d3969b05b29552a6d18b',
+			'0x00be45fe5903ab1b33a9d3969b05b29552a6d18c'
+		];
 		const custodians = {
 			'0x00be45fe5903ab1b33a9d3969b05b29552a6d18b': {
+				code: 'MOZART-PPT',
+				states: {
+					alpha: 0.5,
+					beta: 1,
+					createCommRate: 0.01,
+					ethCollateral: 12.358884504282397,
+					feeBalance: 0.15089954869517602,
+					iterationGasThreshold: 65000,
+					lastOperationTime: 0,
+					lastPrice: 139.65558504384992,
+					lastPriceTime: 1546412400000,
+					limitLower: 0.25,
+					limitPeriodic: 0,
+					limitUpper: 1.75,
+					maturity: 0,
+					minBalance: 0.01,
+					navA: 0.8643764401469363,
+					navB: 1.2712471197061275,
+					nextResetAddrIndex: 0,
+					operationCoolDown: 86400000,
+					period: 3600000,
+					periodCoupon: 0,
+					preResetWaitingBlocks: 10,
+					priceFetchCoolDown: 3000000,
+					redeemCommRate: 0.01,
+					resetPrice: 122.97700574468506,
+					resetPriceTime: 1545530400000,
+					resetState: 'UpwardReset',
+					state: 'Trading',
+					totalSupplyA: 506.61953689367846,
+					totalSupplyB: 1013.2390737873569,
+					totalUsers: 13
+				}
+			},
+			'0x00be45fe5903ab1b33a9d3969b05b29552a6d18c': {
 				code: 'MOZART-PPT',
 				states: {
 					alpha: 0.5,
@@ -53,7 +94,19 @@ describe('BalanceCard Test', () => {
 		};
 		const custodianTokenBalances = {
 			'0x00be45fe5903ab1b33a9d3969b05b29552a6d18b': {
-				LETH: {
+				abc: {
+					balance: 0,
+					allowance: 0,
+					address: '0x59E6B3d43F762310626d2905148939973db2BBd3'
+				},
+				sETH: {
+					balance: 0,
+					allowance: 1.157920892373162e59,
+					address: '0x8a3beca74e0e737460bde45a09594a8d7d8c9886'
+				}
+			},
+			'0x00be45fe5903ab1b33a9d3969b05b29552a6d18c': {
+				abc: {
 					balance: 0,
 					allowance: 0,
 					address: '0x59E6B3d43F762310626d2905148939973db2BBd3'
@@ -115,6 +168,91 @@ describe('BalanceCard Test', () => {
 			expect(wrapEther.mock.calls).toMatchSnapshot();
 			expect(unwrapEther.mock.calls).toMatchSnapshot();
 			expect(notify.mock.calls).toMatchSnapshot();
+		});
+
+		it('Test Negative Input', () => {
+			util.formatMaturity = jest.fn(() => '1970-01-01 08:00:00');
+			util.formatExpiry = jest.fn(() => '1970-01-01 19:00:00');
+			util.getUTCNowTimestamp = jest.fn(() => 1234567890);
+			const wrapEther = jest.fn(() => Promise.resolve('txHash'));
+			const unwrapEther = jest.fn(() => Promise.resolve('txHash'));
+			const notify = jest.fn();
+			const wrapper = shallow(
+				<BalanceCard
+					visible={visible}
+					account={'account'}
+					ethPrice={ethPrice}
+					beethovenList={beethovenList}
+					ethBalance={ethBalance}
+					mozartList={mozartList}
+					custodians={custodians}
+					custodianTokenBalances={custodianTokenBalances}
+					notify={notify}
+					handleClose={handleClose}
+					wrapEther={wrapEther}
+					unwrapEther={unwrapEther}
+				/>
+			);
+			expect(wrapper).toMatchSnapshot();
+			wrapper
+				.find(SInput)
+				.at(0)
+				.simulate('change', { target: { value: '-123456' } });
+			wrapper
+				.find(SInput)
+				.at(1)
+				.simulate('change', { target: { value: '-123456' } });
+			expect(wrapper).toMatchSnapshot();
+			wrapper
+				.find(SButton)
+				.at(1)
+				.simulate('click');
+			wrapper
+				.find(SButton)
+				.at(0)
+				.simulate('click');
+			expect(wrapEther.mock.calls).toMatchSnapshot();
+			expect(unwrapEther.mock.calls).toMatchSnapshot();
+			expect(notify.mock.calls).toMatchSnapshot();
+		});
+
+		it('Test window.open', () => {
+			util.formatMaturity = jest.fn(() => '1970-01-01 08:00:00');
+			util.formatExpiry = jest.fn(() => '1970-01-01 19:00:00');
+			util.getUTCNowTimestamp = jest.fn(() => 1234567890);
+			window.open = jest.fn();
+			const wrapEther = jest.fn(() => Promise.resolve('txHash'));
+			const unwrapEther = jest.fn(() => Promise.resolve('txHash'));
+			const notify = jest.fn();
+			const wrapper = shallow(
+				<BalanceCard
+					visible={visible}
+					account={'account'}
+					ethPrice={ethPrice}
+					beethovenList={beethovenList}
+					ethBalance={ethBalance}
+					mozartList={mozartList}
+					custodians={custodians}
+					custodianTokenBalances={custodianTokenBalances}
+					notify={notify}
+					handleClose={handleClose}
+					wrapEther={wrapEther}
+					unwrapEther={unwrapEther}
+				/>
+			);
+			expect(wrapper).toMatchSnapshot();
+
+			wrapper
+				.find('img')
+				.at(0)
+				.simulate('click');
+			expect(window.open).toBeCalled();
+
+			wrapper
+				.find('img')
+				.at(1)
+				.simulate('click');
+			expect(window.open).toBeCalled();
 		});
 	});
 });
