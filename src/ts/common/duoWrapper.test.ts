41ae66a8081178f95a40993635eae687722f8080
yizhoucao <yizhou.cao@finbook.co>
2019-02-08 13:39:50 +0800

    add test

--- src/ts/common/duoWrapper.test.ts
+++ src/ts/common/duoWrapper.test.ts
@@ -4,20 +4,20 @@ jest.mock('@finbook/duo-contract-wrapper', () => ({
 	Web3Wrapper: jest.fn(() => 'web3Wrapper'),
 	DualClassWrapper: jest.fn(() => ({
 		contract: 'dualClassWrapper'
-	})),
-	EsplanadeWrapper: jest.fn(() => ({
-		contract: 'EsplanadeWrapper'
-	})),
-	MagiWrapper: jest.fn(() => ({
-		contract: 'MagiWrapper'
 	}))
 }));
 
-import { Web3Wrapper } from '@finbook/duo-contract-wrapper';
+import { DualClassWrapper, Web3Wrapper } from '@finbook/duo-contract-wrapper';
 
-import { duoWeb3Wrapper } from './duoWrapper';
+import { dualClassWrappers, duoWeb3Wrapper, getDualClassWrapper } from './duoWrapper';
 
-test('web3Wrapper', () => {
+test('Web3Wrapper', () => {
 	expect(duoWeb3Wrapper).toBeTruthy();
 	expect((Web3Wrapper as any).mock.calls).toMatchSnapshot();
+});
+
+test('DualClassWrapper', () => {
+	getDualClassWrapper('custodian');
+	expect(dualClassWrappers).toMatchSnapshot();
+	expect((DualClassWrapper as any).mock.calls).toMatchSnapshot();
 });
